f2dff736c1c4e6dfe6d400802e650e275f3cc3e6
Stainamou <margussten@gmail.com>
2025-08-26 15:04:09 +0300

    commit 2

--- untitled/src/main/java/observer/WeatherStation.java
+++ untitled/src/main/java/observer/WeatherStation.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import observer.WeatherObserver;
 
-public abstract class WeatherStation {
+public abstract class WeatherStation extends Thread{
     private List<WeatherObserver> weatherObservers = new ArrayList<>();
+
+    @Override
+    public void run() {
+
+    }
 
     public void addObserver(WeatherObserver observer) {
         weatherObservers.add(observer);
@@ -15,6 +20,9 @@ public abstract class WeatherStation {
         weatherObservers.remove(observer);
     }
 
-    public void notifyObservers() {
+    public void notifyObservers(int temperature) {
+        for (WeatherObserver observer : weatherObservers) {
+            observer.updateTemperature(temperature);
+        }
     }
 }
